0bdee9cecad2b1fc0c419dff40bef67560885cad
tigerbyby <71328569+tigerbyby@users.noreply.github.com>
2020-09-15 14:46:41 +0200

    Create antiencrypt.sh

---
+++ antiencrypt.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#=====================================================================
+# This scipt parse smbd_audit log file. Experimental variant
+# You need to add 
+# log level = 0 vfs:2 
+# into smb.conf. And
+# :msg, contains, "smbd_audit"    /var/log/smbd_audit/smbd_audit.log
+# :msg, contains, "smbd_audit"    stop
+# into /etc/rsyslog.conf
+#=====================================================================
+
+tmp=/tmp/smbcheck
+emails="test@exammple.com"
+log=/var/log/smbd_audit/smbd_audit.log
+
+#=====================================================================
+
+touch $tmp
+while true; do
+    curtime=$(date +%T)
+    tail -n50 $log | \
+    sed -r 's/ ?//g' | \
+    awk -F '|' '$4=="rename" && $1~/'$curtime'/ {print $3" "$6" "$7}' | \
+    while read ip bef aft; do
+        bef=$(dirname $bef)
+        aft=$(dirname $aft)
+        if [ $bef = $aft ]; then
+            echo $ip >> $tmp
+        fi
+    done
+    cat $tmp | sort | uniq -c | \
+    while read cnt ip; do
+        if [ "$cnt" -ge 3 ]; then
+            echo $ip
+            isb=$(iptables -L -nv | awk '/'$ip'/ && /445/ {print $NF}')
+            if [ -z "$isb" ]; then
+                iptables -I INPUT -p tcp -m multiport -s $ip --dports 139,445 -j DROP
+                echo "Alert!!! blocked $ip for $cnt renames/sec" | \
+                mail -s "[$(hostname -f)] encrypor blocked!!!" $emails
+            fi
+        fi
+    done
+    > $tmp
+    sleep 0.2
+done
